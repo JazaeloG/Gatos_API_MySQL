4f8fe9e38a45a64ef5706751d1fa9ed1300d8cd4
Jazael Galindo <115960860+JazaeloG@users.noreply.github.com>
2023-05-05 20:10:15 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 
 const server= require('./server');
 
-const PORT = process.env.PORT || 3330;
+const PORT = process.env.PORT || 3300;
 
 server.listen(PORT, ()=> console.log(`Server encendido en puerto: ${PORT}`));
